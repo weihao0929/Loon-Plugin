8933a23dc493ec18c533d4639121cd18f36a1533
Ayden <55398569+weihao0929@users.noreply.github.com>
2026-01-05 10:43:13 +0800

    更新 xmind.js

--- Xmind/xmind.js
+++ Xmind/xmind.js
@@ -1,19 +1,35 @@
 /*
-脚本功能：Xmind 旧版解锁逻辑 (2022版)
-说明：针对新版可能已失效，仅作测试。
+脚本功能：Xmind 解锁 (chxm1023 双重验证版)
+脚本作者：chxm1023
+修改人员：Ayden
+使用说明：⚠️必须先在 App 内登录账号，然后点击“恢复购买”触发脚本。
 */
 
-// 解析响应体
-let obj = JSON.parse($response.body);
+const url = $request.url;
+let body = $response.body;
+let obj = JSON.parse(body);
 
-// 如果存在 license 字段，则进行修改
-if (obj.license) {
-  // 修改状态为 'sub' (subscription)
-  obj.license.status = 'sub';
-  
-  // 修改过期时间为 2030-01-11 (16进制 0x1b8d90e4000 转 10进制)
-  obj.license.expireTime = 1894255534080;
+// 定义匹配正则
+const vipPath = /_res\/devices/;
+const tokenPath = /_res\/token/;
+
+// 场景1：处理设备/会员状态
+if (vipPath.test(url)) {
+    obj = {
+      "license" : {
+        "status" : "sub",
+        "expireTime" : 4092599349000
+      },
+      "_code" : 200,
+      // 关键签名数据
+      "raw_data" : "GfxYgAqnrQ/ggD9UwqnZyAj6FKnopXzM8s5m3eZLOsmVr4FwCzv41qTmgi/u72cv+jpaAoljaEPti1twzOS7X7KUPY1KNJ2xalbS7SXbvFHSvs21QXjaUtIOkeJWAl4/vHl4IvMeHTBVqD8mFCXOmvnBPLRMAJEPgHEJYF1InvQ="
+    };
 }
 
-// 重新打包并结束
-$done({body: JSON.stringify(obj)});
+// 场景2：处理 Token 验证
+if (tokenPath.test(url)) {
+    obj.expireDate = 4092599349;
+    obj.token = "f50633ea8eb04cbb85962b99c47045d7AjOobEGo";
+}
+
+$done({body : JSON.stringify(obj)});
